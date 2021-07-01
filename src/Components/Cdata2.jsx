aaefa1301b8850d5607e81f8f065914e51b7c017
Vivek kushwah <46090818+vivekkushwah16@users.noreply.github.com>
2021-07-01 22:31:03 +0530

    Add files via upload

---
+++ src/Components/Cdata2.jsx
@@ -0,0 +1,21 @@
+const Cdata2=[
+    {   id:1,
+        imasrc:"https://labhansh-website-reactjs-01.herokuapp.com/images/team/team-1.jpg",
+        title:"Walter White",
+        detail:" Founder and CEO",     
+    },
+    {   id:2,
+        imasrc:"https://labhansh-website-reactjs-01.herokuapp.com/images/team/team-2.jpg",
+            title:"Sarah Jhonson",
+            detail:"Product Manager", 
+            
+    },
+    {     id:3,
+         imasrc:"https://labhansh-website-reactjs-01.herokuapp.com/images/team/team-3.jpg",
+            title:"William Anderson",
+            detail:"Senior Developer", 
+            
+    }
+    
+]
+export default Cdata2;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactDom from "react-dom";
+import App  from "./Components/App";
+reactDom.render(
+    <>
+      <App/>   
+    </>
+    ,document.getElementById("root")
+    
+);
+    
+       
